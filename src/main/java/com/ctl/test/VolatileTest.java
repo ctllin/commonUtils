143a86256f83847450f6ec1390a6616009c11abb
林家小院 <guolinit@163.com>
2018-12-03 11:59:37 +0800

    mvn

--- src/main/java/com/ctl/test/VolatileTest.java
+++ src/main/java/com/ctl/test/VolatileTest.java
@@ -13,31 +13,32 @@ import java.util.concurrent.TimeUnit;
  * @date 2018-12-03 11:46
  */
 public class VolatileTest {
-    final static int MAX=5;
-    static int init_value=0;
+    final static int MAX = 5;
+    //static volatile int init_value = 0;
+    static  int init_value = 0;
 
     public static void main(String[] args) {
-    new Thread(()->{
-        int localValue  = init_value;
-        while (localValue<MAX){
-            if(init_value!=localValue){
-                System.out.printf("The init_value is updated to[%d]\n",init_value);
-                localValue=init_value;
+        new Thread(() -> {
+            int localValue = init_value;
+            while (localValue < MAX) {
+                if (init_value != localValue) {
+                    System.out.printf("The init_value is updated to[%d]\n", init_value);
+                    localValue = init_value;
+                }
             }
-        }
-    },"reader").start();
-        new Thread(()->{
-            int localValue  = init_value;
-            while (localValue<MAX){
+        }, "reader").start();
+        new Thread(() -> {
+            int localValue = init_value;
+            while (localValue < MAX) {
 
-                    System.out.printf("The init_value will be change to[%d]\n",++localValue);
-                    init_value=localValue;
+                System.out.printf("The init_value will be change to[%d]\n", ++localValue);
+                init_value = localValue;
             }
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        },"updater").start();
+        }, "updater").start();
     }
 }
